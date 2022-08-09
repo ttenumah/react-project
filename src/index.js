cf33ca15e40fc485b677cb2045eb7383f908b389
ttenumah <ttenumah@gmail.com>
2022-08-09 13:34:07 -0700

    ff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
