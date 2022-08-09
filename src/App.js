d8540a24447263cba4c26c5532ade32b1c30439a
ttenumah <ttenumah@gmail.com>
2022-08-09 09:52:42 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./index.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      
     </div>
   );
 }
