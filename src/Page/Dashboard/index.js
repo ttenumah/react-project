69b9eebb2bc9e86afed936a0109c67e048260f53
ttenumah <ttenumah@gmail.com>
2022-08-09 12:49:43 -0700

    b

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -1,10 +1,65 @@
-import react from 'react'
+import React, {useState} from 'react'
 
+import Swal from 'sweetalert2';
+
+import Header from './Header';
+import ListEmployee from './ListEmployee';
+import AddEmployee from './AddEmployee';
+import EditEmployee from './EditEmployee';
 import { employeesData } from '../../data';
 
 function Dashboard() {
+    const [employees, setEmployees] = useState(employeesData);
+    const [SelectedEmployee, setSeletedEmployee] = useState(null);
+    const [isAdding, setisAdding] = useState(false);
+    const [isEditing, setiEditing] = useState(false);
+    
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = () => {
+        
+    }
+
     return(
-        <div>Dashboard </div>
+        <div className='container'>
+            {/*List */}
+            {!isAdding && !isEditing && (
+                <>
+            <Header
+            setAdding={setAdding}
+            />
+            <ListEmployee
+            employees={employees}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+            />
+            </>
+            )
+            }
+{/*add*/}
+         {isAdding&& (
+            <Add
+            employees={employees}
+            setEmployees={setEmployees}
+            setisAdding={setisAdding}
+            />
+            )
+            }
+
+{/*Edit*/}
+{isEditing && (
+            <Edit
+            employees={employees}
+            setEmployees={setEmployees}
+            setiEditing={setiEditing}
+            SelectedEmployee={setSeletedEmployee}
+            />
+            )
+            }
+
+        </div>
     )
 }
 
