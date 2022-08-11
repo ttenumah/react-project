72dffa9e7af1cb5af3769e3edf9b483b92fd2dbb
ttenumah <ttenumah@gmail.com>
2022-08-11 16:19:24 -0700

    tb

--- src/Page/Dashboard/ListEmployee.js
+++ src/Page/Dashboard/ListEmployee.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 function ListEmployee() {
   return (
-    <div>ListEmployee</div>
+    <div><table>
+    <thead>
+      <tr>
+        <th>Head 1</th>
+        <th>Head 2</th>
+        <th>Head 3</th>
+      </tr>
+    </thead>
+    <tfoot>
+      <tr>
+        <th>Footer 1</th>
+        <th>Footer 2</th>
+        <th>Footer 3</th>
+      </tr>
+    </tfoot>
+    <tbody>
+      <tr>
+        <td>Description 1</td>
+        <td>Description 2</td>
+        <td>Description 3</td>
+      </tr>
+      <tr>
+        <td>Description 1</td>
+        <td>Description 2</td>
+        <td>Description 3</td>
+      </tr>
+      <tr>
+        <td>Description 1</td>
+        <td>Description 2</td>
+        <td>Description 3</td>
+      </tr>
+    </tbody>
+  </table></div>
   )
 }
 
