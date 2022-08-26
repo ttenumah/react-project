d3169d24a3d95b5b8692723128bced083fec1367
ttenumah <ttenumah@gmail.com>
2022-08-26 07:50:34 -0700

    ttt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-import Dashboard2 from './Page/Dashboard';
-import Dashboard from './Page/Dashboard';
 
-function App() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import LandingPage from './Page/Dashboard/AddEmployee'
+import Login from './Page/Dashboard/Login'
+import RegisterPage from './Page/Dashboard/ListEmployee'
+import ForgetPasswordPage from './Page/Dashboard/Header'
+import HomePage from './Page/Dashboard/EditEmployee'
+
+
+import './App.css'
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    
+                    <Route exact path="/Login" component={ Login } />
+                    <Route path="/register" component={ RegisterPage } />
+                    <Route path="/forget-password" component={ ForgetPasswordPage } />
+                    <Route path="/home" component={ HomePage } />
+                </Routes>
+            
+            </div>
+        </Router>
+    )
 }
-
-export default App;
